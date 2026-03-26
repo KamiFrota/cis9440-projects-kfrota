a002978b6b22cfd21552831d11cd94360cd55fb4
Kami Frota <kami.frota@gmail.com>
2026-03-26 05:17:12 +0000

    removed filter date > 7 years from both staging tables

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -74,7 +74,6 @@ cleaned AS (
    -- Filters
    WHERE objectid IS NOT NULL
    AND time_of_submission IS NOT NULL
-   AND CAST(time_of_submission AS DATE) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 YEAR)
    AND borough IS NOT NULL
 
    -- Deduplicate
